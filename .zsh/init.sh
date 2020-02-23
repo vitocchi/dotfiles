e6cc4de3a946f6d49aa3aebf1b381f22c0e33475
vitocchi <vitocchi.mm@gmail.com>
2020-02-23 16:25:09 +0900

    update

--- .zsh/init.sh
+++ .zsh/init.sh
@@ -2,6 +2,6 @@ if [ ! -e "${ZDOTDIR:-$HOME}/.zprezto" ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do  
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"  
 done
