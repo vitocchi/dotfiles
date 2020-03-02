7b789ac1e0949ec44b5975e03be20ee9dba95354
vitocchi <vitocchi.mm@gmail.com>
2020-03-02 23:17:02 +0900

    update

--- .karabiner/init.sh
+++ .karabiner/init.sh
@@ -1,2 +1,2 @@
 cd `dirname $0`
-ln -sf ~/dotfiles/karabiner/custom.json ~/.config/karabiner/assets/complex_modifications/custom.json
+ln -sf ~/dotfiles/.karabiner/custom.json ~/.config/karabiner/assets/complex_modifications/custom.json
